453cbe8d18de4dbe8a1093003b79a0278b11783d
Kacper Łukawski <ka.lukawski@gmail.com>
2017-01-15 22:46:11 +0100

    Added logic

--- app/index.js
+++ app/index.js
@@ -1,2 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function sum (num, total) {
+    return total + num
+  }
+  function sub (num, total) {
+    return total - num
+  }
+  function div (num, total) {
+    if (num === 0) return false
+    return total / num
+  }
+  function mult (num, total) {
+    return total * num
+  }
 
+  var operations = {
+    sum: sum,
+    sub: sub,
+    div: div,
+    mult: mult
+  }
+
+  var result = 0
+
+  var userInput = document.getElementById('userInput')
+  var resultBox = document.getElementById('result')
+
+  var btns = document.getElementsByClassName('operations')
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function (e) {
+      result = (this.getAttribute('data-operation') === 'equal') ? result : operations[this.getAttribute('data-operation')](Number(userInput.value), result)
+      if (this.getAttribute('data-operation') === 'equal') resultBox.innerText = result
+      else resultBox.innerText += ' ' + userInput.value + ' ' + this.innerText
+    })
+  }
+})
 
