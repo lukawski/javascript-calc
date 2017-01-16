a84496896cbca00e96ccac01859468c12dcaa28c
Kacper Łukawski <ka.lukawski@gmail.com>
2017-01-16 22:34:32 +0100

    Meh

--- app/index.js
+++ app/index.js
@@ -22,8 +22,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var result = 1
   var currentNum = 0
-  var currentOperation = ''
-
   var resultBox = document.getElementById('result')
 
   var btns = document.getElementsByClassName('operations')
@@ -31,26 +29,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function (e) {
-      currentOperation = this.getAttribute('data-operation')
-      if (currentOperation === 'equal') {
-        resultBox.innerText = result
-      } else if (currentOperation === 'clear') {
-        resultBox.innerText = ''
-        currentNum = 0
-        result = 1
-        currentOperation = ''
-      } else {
-        resultBox.innerText += this.innerText
-      }
+
     })
   }
 
   for (var j = 0; j < digits.length; j++) {
     digits[j].addEventListener('click', function (e) {
-      currentNum = Number(this.innerText)
-      if (currentOperation !== '') result = operations[currentOperation](currentNum, result)
-      else result = currentNum
-      resultBox.innerText += currentNum
+
     })
   }
 })
