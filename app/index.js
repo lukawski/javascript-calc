d2e12b5dc51d4efb65dc768060ebc08564adcbab
Kacper Łukawski <ka.lukawski@gmail.com>
2017-01-17 17:33:14 +0100

    Simple design

--- app/index.js
+++ app/index.js
@@ -1,27 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-  function sum (num, total) {
-    return total + num
-  }
-  function sub (num, total) {
-    return total - num
-  }
-  function div (num, total) {
-    if (num === 0) return false
-    return total / num
-  }
-  function mult (num, total) {
-    return total * num
-  }
-
-  var operations = {
-    sum: sum,
-    sub: sub,
-    div: div,
-    mult: mult
-  }
-
   var result = 1
-  var currentNum = 0
+  var chain = ''
   var resultBox = document.getElementById('result')
 
   var btns = document.getElementsByClassName('operations')
@@ -29,13 +8,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function (e) {
+      if (this.getAttribute('data-operation') === 'equal') {
+        result = eval(chain)
+        resultBox.innerText = result
+        chain = result
+        return true
+      }
 
+      if (this.getAttribute('data-operation') === 'clear') {
+        resultBox.innerText = ''
+        chain = ''
+        return true
+      }
+
+      resultBox.innerText += this.innerText
+      chain += this.getAttribute('data-operation')
     })
   }
 
   for (var j = 0; j < digits.length; j++) {
     digits[j].addEventListener('click', function (e) {
-
+      resultBox.innerText += this.innerText
+      chain += this.innerText
     })
   }
 })
